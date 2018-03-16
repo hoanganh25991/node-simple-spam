e4235bbcda801fffcb59126090a068dab2f5a431
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-03-16 20:22:45 +0700

    loop

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import Oxios from "axios"
 
-const axios = Oxios.create({timeout: 300})
+const axios = Oxios.create({timeout: 10000})
 const _ = console.log
-const threshold = 10000
+const threshold = 1000
 const site = "http://hahuyetapcao.com/"
 
+let count = 0
+let wait = Promise.resolve()
+
 const call = async () => {
+  _("[count]", count)
+
   try{
     await axios.get(site)
-  }catch(err){}
+    _("[call] success")
+  }catch(err){
+    _("[call] fail")
+  }finally {
+    count--
+  }
 }
 
-const loop = async () => {
-  const list = new Array(threshold).fill(null)
-  await Promise.all(list.map(() => call()))
-  _("[re-loop] at", new Date().toString())
-  await loop()
+const loop = () => {
+  do{
+    count++
+    const waitCall = call()
+    wait = wait.then(() => waitCall)
+  }while(count < threshold)
+  loop()
 }
 
 
 (async() => {
-  await loop()
+  loop()
+  await wait
 })()
